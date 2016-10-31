99c818bee4454bdc4976d0ce921d7cd9714450af
Lihacovs <k.lihacovs@inbox.lv>
2016-10-31 21:05:34 +0000

    Criminal Intent base

---
+++ app/src/main/java/com/lihacovs/android/criminalintent/CrimeListActivity.java
@@ -0,0 +1,15 @@
+package com.lihacovs.android.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Konstantins on 10/31/2016.
+ */
+
+public class CrimeListActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return new CrimeListFragment();
+    }
+}
