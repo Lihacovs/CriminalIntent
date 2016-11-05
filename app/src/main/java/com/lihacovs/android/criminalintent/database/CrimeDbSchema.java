c8b7460026f10dcea1d808d927f93055b616b4b0
Lihacovs <k.lihacovs@inbox.lv>
2016-11-05 13:46:08 +0000

    SQLite db

---
+++ app/src/main/java/com/lihacovs/android/criminalintent/database/CrimeDbSchema.java
@@ -0,0 +1,18 @@
+package com.lihacovs.android.criminalintent.database;
+
+/**
+ * Created by Konstantins on 11/5/2016.
+ */
+
+public class CrimeDbSchema {
+    public static final class CrimeTable {
+        public static final String NAME = "crimes";
+
+        public static final class Cols {
+            public static final String UUID = "uuid";
+            public static final String TITLE = "title";
+            public static final String DATE = "date";
+            public static final String SOLVED = "solved";
+        }
+    }
+}
